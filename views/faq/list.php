60eca54976368a4bf9ee6d059b9a83228fee693c
usesgraphcrt <usesgraphcrt@gmail.com>
2016-12-20 15:05:37 +0500

    no message

--- views/faq/list.php
+++ views/faq/list.php
@@ -14,38 +14,47 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="faq-view">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-2 mini-menu">
-                <ul>
+            <div class="col-md-3">
+                <div class="panel-group" id="accordion">
                     <?php
-
                     if ($categories) {
-                        foreach ($categories as $category) {
+                    foreach ($categories as $category) {
 
-                            if ($category->getFaq()->all()) {
-                                ?>
-                                <li class="sub">
-                                    <a href="#"><?= $category->name ?></a>
-                                    <ul>
-                                        <?php
-                                        foreach ($category->getFaq()->all() as $faq) { ?>
-                                            <li><a class="btn" data-role="faq-load"
-                                                   data-url="<?= \yii\helpers\Url::to(['faq/ajax-list-view', 'id' => $faq->id]) ?>"><?= $faq->title ?></a>
-                                            </li>
+                    if ($category->getFaq()->all()) {
+                    ?>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse<?= $category->id ?>"><?= $category->name ?></a>
+                            </h4>
+                        </div>
+                        <div id="collapse<?= $category->id ?>" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                <ul class="nav nav-pills nav-stacked">
+                                <?php
+                                foreach ($category->getFaq()->all() as $faq) { ?>
+                                       <li>
+                                           <a class="panel-content" data-role="faq-load"
+                                                       data-url="<?= \yii\helpers\Url::to(['faq/ajax-list-view', 'id' => $faq->id]) ?>"><?= $faq->title ?>
+                                           </a>
+                                       </li>
+
                                             <?php
                                         }
                                         ?>
-                                    </ul>
-                                </li>
-                                <?php
-                            }
-                        }
-                        ?>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <?php }
+                    }
+                    ?>
                     <?php } else {?>
                         <h4><?= Module::t('faq', 'NO_DATA'); ?></h4>
                     <?php } ?>
-                </ul>
+                </div>
             </div>
-            <div class="faq-view col-md-10" data-role="faq-view">
+            <div class="faq-view col-md-9" data-role="faq-view">
 
             </div>
         </div>
